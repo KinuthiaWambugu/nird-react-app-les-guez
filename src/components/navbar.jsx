4f43b8ea089ca5339e1a66ce0f1aee93dbffea94
theyloveBMNEG7 <tchokoanssiemmanuel@gmail.com>
2025-12-04 23:20:33 +0100

    Add navbar, footer, Home and Digital Village page

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">        
+        <div className="navbar-brand">
+            <Link to="/">Project Logo</Link>
+        </div>
+        <ul className="navbar-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/digital_village">Digital Village</Link></li>
+            <li><Link to="/quiz">Quiz</Link></li>
+            <li><Link to="/nird">NIRD Resources</Link></li>
+        </ul>
+    </nav>
+    );
+}
+
+export default Navbar;
