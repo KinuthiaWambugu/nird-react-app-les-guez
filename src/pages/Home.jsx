5229ad94aad8600bc669d8f4aa1309dd78d4b8d7
Kinuthia Wambugu <kinuthiaandrew713@gmail.com>
2025-12-04 23:30:43 +0100

    Made hero part in Threescene js file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThreeJSScene from "../components/home/ThreeJSScene"
+
+const Home = () =>{
+    return (
+        <div>
+            <ThreeJSScene />
+        </div>
+    )
+}
+
+export default Home;
